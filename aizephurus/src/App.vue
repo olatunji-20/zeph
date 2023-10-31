dd2b3a896a71170a7db8ba70a4da8bdd461869d2
Falana Sheriff <Falanasheriffdeen@gmail.com>
2023-10-31 19:06:57 +0100

    Update App.vue

--- aizephurus/src/App.vue
+++ aizephurus/src/App.vue
@@ -16,7 +16,7 @@
         <i class="embrace">Your success is our success!</i>
         <h4>Embrace the Future Today</h4>
         <p>Don't be left behind; embrace the future of customer engagement with our AI-powered chatbots for businesses. Transform the way you interact with your customers, drive
-          revenue growth, and outshine your competition gfghhh ggjif bgfgcompetition and gfhhhwith the power of artificial intelligence.
+          revenue growth, and outshine your bdhd heve heve twgcompetition gfghhh ggjif bgfgcompetition and gfhhhwith the power of artificial intelligence.
         </p>
       </article>
     </div>
